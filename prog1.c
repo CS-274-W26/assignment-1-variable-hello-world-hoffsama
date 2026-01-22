8c5be3b9cd453c25420622ee77d9b218d21f0e00
Samantha Hoffmann <samantha.lynn.hoffmann@gmail.com>
2026-01-21 19:10:38 -0800

    Completed the assignment

--- prog1.c
+++ prog1.c
@@ -1,11 +1,26 @@
 #include <stdio.h> // Necessary for printf()
-
-// TODO Other includes
+#include <stdlib.h>
+#include <time.h>
 
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
-	
-	// TODO Complete the program
+
+    srand(time(NULL)); //seed random number gen
+    
+    int myrand = rand()%11; // should output 0-10
+
+    if (myrand < 5) {
+        printf("Eat more beef, kick less cats\n");
+
+    } else if (myrand < 10) {
+        printf("FRODO LIVES\n");
+
+    } else {
+        printf("Larn is the best roguelike\n");
+
+    }
+
+    printf("The random number was: %d\n", myrand); //tell user num regardless
+
+    return 0;
 }
